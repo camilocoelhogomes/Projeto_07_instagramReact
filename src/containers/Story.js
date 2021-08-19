df16a7d8ada3977eadc4175f0b89474d199232a9
camilocoelhogomes <camilo.coelho.gomes@gmail.com>
2021-08-19 13:59:33 -0300

    submoduling the react app

---
+++ src/containers/Story.js
@@ -0,0 +1,14 @@
+const Story = () => {
+    return (
+        <div class="story">
+            <div class="imagem">
+                <img src="assets/img/9gag.svg" />
+            </div>
+            <div class="usuario">
+                9gag
+            </div>
+        </div>
+    );
+}
+
+export default Story;
---
+++ src/containers/Sugestao.js
@@ -0,0 +1,17 @@
+const Sugestao = () => {
+    return (
+        <div class="sugestao">
+            <div class="usuario">
+                <img src="assets/img/bad.vibes.memes.svg" />
+                <div class="texto">
+                    <div class="nome">bad.vibes.memes</div>
+                    <div class="razao">Segue você</div>
+                </div>
+            </div>
+
+            <div class="seguir">Seguir</div>
+        </div>
+    );
+}
+
+export default Sugestao;
