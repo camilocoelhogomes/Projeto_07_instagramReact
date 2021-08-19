abe454e2a3f019c57b32f11f0544f9e188dc20ba
camilocoelhogomes <camilo.coelho.gomes@gmail.com>
2021-08-19 14:34:14 -0300

    betterin the topic subdivision

--- src/containers/Corpo.js
+++ src/containers/Corpo.js
@@ -1,74 +1,16 @@
-import Story from "./Story";
-import Post from "./Post";
-import Sugestao from "./Sugestao";
-const Stories = () => {
-    return (
-        <div class="stories">
-            <Story />
-
-            <div class="setinha">
-                <ion-icon name="chevron-forward-circle"></ion-icon>
-            </div>
-        </div>
-
-    );
-}
-
-const Posts = () => {
-    return (
-        <div class="posts">
-            <Post />
-        </div>
-
-    );
-}
-
-const Esquerda = () => {
-    return (
-        <div class="esquerda">
-            <Stories />
-            <Posts />
-        </div>
-    );
-}
-
-
-const SideBar = () => {
-    return (
-        <div class="sidebar">
-            <div class="usuario">
-                <img src="assets/img/catanacomics.svg" />
-                <div class="texto">
-                    <strong>catanacomics</strong>
-                    Catana
-                </div>
-            </div>
-
-            <div class="sugestoes">
-                <div class="titulo">
-                    Sugestões para você
-                    <div>Ver tudo</div>
-                </div>
-
-                <Sugestao />
-
-            </div>
-
-            <div class="links">
-                Sobre • Ajuda • Imprensa • API • Carreiras • Privacidade • Termos • Localizações • Contas mais relevantes • Hashtags • Idioma
-            </div>
-
-            <div class="copyright">
-                © 2021 INSTAGRAM DO FACEBOOK
-            </div>
-        </div>
-    );
-}
+import Stories from "./Stories";
+import Posts from "./Posts";
+import SideBar from "./SideBar";
 
 const Corpo = () => {
     return (
         <div class="corpo">
-            <Esquerda />
+
+            <div class="esquerda">
+                <Stories />
+                <Posts />
+            </div>
+
             <SideBar />
         </div>
     );
