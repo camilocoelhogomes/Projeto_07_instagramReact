53258362c7a5657eebd779d5b8a53e02b5cdaf79
camilocoelhogomes <camilo.coelho.gomes@gmail.com>
2021-08-21 10:42:12 -0300

    posts rendering by props

--- src/containers/Posts.js
+++ src/containers/Posts.js
@@ -1,18 +1,11 @@
-const Post = () => {
-
-    const userName = 'meowed';;
-    const profileImg = 'assets/img/meowed.svg';
-    const postContent = 'assets/img/gato-telefone.svg';
-    const likeByUser = 'respondeai';
-    const likeByImg = "assets/img/respondeai.svg";
-    const numberOfLikes = 101.523;
+const Post = (props) => {
 
     return (
         <div class="post">
             <div class="topo">
                 <div class="usuario">
-                    <img src={profileImg} />
-                    {userName}
+                    <img src={props.profileImg} />
+                    {props.userName}
                 </div>
                 <div class="acoes">
                     <ion-icon name="ellipsis-horizontal"></ion-icon>
@@ -20,7 +13,7 @@ const Post = () => {
             </div>
 
             <div class="conteudo">
-                <img src={postContent} />
+                <img src={props.postContent} />
             </div>
 
             <div class="fundo">
@@ -36,9 +29,9 @@ const Post = () => {
                 </div>
 
                 <div class="curtidas">
-                    <img src={likeByImg} />
+                    <img src={props.likeByImg} />
                     <div class="texto">
-                        Curtido por <strong>{likeByUser}</strong> e <strong>outras {numberOfLikes} pessoas</strong>
+                        Curtido por <strong>{props.likeByUser}</strong> e <strong>outras {props.numberOfLikes} pessoas</strong>
                     </div>
                 </div>
             </div>
@@ -47,9 +40,36 @@ const Post = () => {
 }
 
 const Posts = () => {
+    const dados = [
+        {
+            userName: 'meowed',
+            profileImg: 'assets/img/meowed.svg',
+            postContent: 'assets/img/gato-telefone.svg',
+            likeByUser: 'respondeai',
+            likeByImg: "assets/img/respondeai.svg",
+            numberOfLikes: 101.523,
+        },
+        {
+            userName: 'barked',
+            profileImg: 'assets/img/barked.svg',
+            postContent: 'assets/img/dog.svg',
+            likeByUser: 'adorable_animals',
+            likeByImg: "assets/img/adorable_animals.svg",
+            numberOfLikes: 99.159,
+        }
+    ];
+
     return (
         <div class="posts">
-            <Post />
+            {dados.map(item =>
+                <Post userName={item.userName}
+                    profileImg={item.profileImg}
+                    postContent={item.postContent}
+                    likeByImg={item.likeByImg}
+                    likeByUser={item.likeByUser}
+                    numberOfLikes={item.numberOfLikes}
+                />
+            )}
         </div>
 
     );
