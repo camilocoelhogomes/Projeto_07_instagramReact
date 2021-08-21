5685e9baf7a7ec2e2e5706dac8e51d25c7e35a47
camilocoelhogomes <camilo.coelho.gomes@gmail.com>
2021-08-21 10:25:22 -0300

    stories add with classes

--- src/containers/Stories.js
+++ src/containers/Stories.js
@@ -1,24 +1,57 @@
-const Story = () => {
-
-    const img = "assets/img/9gag.svg";
-    const usuario = '9gag'
+const Story = (props) => {
 
     return (
         <div class="story">
             <div class="imagem">
-                <img src={img} />
+                <img src={props.img} />
             </div>
             <div class="usuario">
-                {usuario}
+                {props.usuario}
             </div>
         </div>
     );
 }
 
 const Stories = () => {
+
+    const stories = [
+        {
+            img: "assets/img/9gag.svg",
+            usuario: '9gag'
+        },
+        {
+            img: 'assets/img/meowed.svg',
+            usuario: 'meowed'
+        },
+        {
+            img: 'assets/img/barked.svg',
+            usuario: 'barked'
+        },
+        {
+            img: 'assets/img/nathanwpylestrangeplanet.svg',
+            usuario: 'nathanwpylestrangeplanet'
+        },
+        {
+            img: 'assets/img/wawawicomics.svg',
+            usuario: 'wawawicomics'
+        },
+        {
+            img: 'assets/img/respondeai.svg',
+            usuario: 'respondeai'
+        },
+        {
+            img: 'assets/img/filomoderna.svg',
+            usuario: 'filomoderna'
+        },
+        {
+            img: 'assets/img/memeriagourmet.svg',
+            usuario: 'memeriagourmet'
+        },
+    ]
+
     return (
         <div class="stories">
-            <Story />
+            {stories.map(item => <Story img={item.img} usuario={item.usuario} />)}
 
             <div class="setinha">
                 <ion-icon name="chevron-forward-circle"></ion-icon>
