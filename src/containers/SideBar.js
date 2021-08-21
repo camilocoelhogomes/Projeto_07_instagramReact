fb3380d8a2241d558c925be56edd40537da59c85
camilocoelhogomes <camilo.coelho.gomes@gmail.com>
2021-08-21 11:00:22 -0300

    sugestions part done

--- src/containers/SideBar.js
+++ src/containers/SideBar.js
@@ -1,13 +1,11 @@
-const Sugestao = () => {
-    const img = "assets/img/bad.vibes.memes.svg";
-    const user = "bad.vibes.memes";
+const Sugestao = (props) => {
 
     return (
         <div class="sugestao">
             <div class="usuario">
-                <img src={img} />
+                <img src={props.img} />
                 <div class="texto">
-                    <div class="nome">{user}</div>
+                    <div class="nome">{props.user}</div>
                     <div class="razao">Segue você</div>
                 </div>
             </div>
@@ -17,35 +15,66 @@ const Sugestao = () => {
     );
 }
 
-const Usuario = () => {
-
-    const img = "assets/img/catanacomics.svg";
-    const igName = 'catanacomics';
-    const userName = 'Catana';
+const Usuario = (props) => {
 
     return (
         <div class="usuario">
-            <img src={img} />
+            <img src={props.img} />
             <div class="texto">
-                <strong>{igName}</strong>
-                {userName}
+                <strong>{props.igName}</strong>
+                {props.userName}
             </div>
         </div>
     );
 }
 
 const SideBar = () => {
+
+    const usuario = {
+        img: "assets/img/catanacomics.svg",
+        igName: 'catanacomics',
+        userName: 'Catana',
+    }
+
+    const sugestoes = [
+        {
+            img: "assets/img/bad.vibes.memes.svg",
+            user: "bad.vibes.memes",
+        },
+        {
+            img: "assets/img/chibirdart.svg",
+            user: "chibirdart",
+        },
+        {
+            img: "assets/img/razoesparaacreditar.svg",
+            user: "razoesparaacreditar",
+        },
+        {
+            img: "assets/img/adorable_animals.svg",
+            user: "adorable_animals",
+        },
+        {
+            img: "assets/img/smallcutecats.svg",
+            user: "smallcutecats",
+        },
+    ]
     return (
         <div class="sidebar">
 
-            <Usuario />
+            <Usuario img={usuario.img}
+                igName={usuario.igName}
+                userName={usuario.userName}
+            />
             <div class="sugestoes">
                 <div class="titulo">
                     Sugestões para você
                     <div>Ver tudo</div>
                 </div>
 
-                <Sugestao />
+                {sugestoes.map(sugestao =>
+                    <Sugestao user={sugestao.user}
+                        img={sugestao.img}
+                    />)}
 
             </div>
 
